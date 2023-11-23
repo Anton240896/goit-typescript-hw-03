6f58c93a725f00de865ecf45d2050339d3c763ff
Anton240896 <antonalipov791@gmail.com>
2023-11-23 14:22:49 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,16 @@
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+class Key {
+  private quantity: number = Math.random();
+  getQuantity(): number {
+    return this.quantity;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
